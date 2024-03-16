dde7000826357d4d81aee31951a5be24fc0b39bb
quhaoze <quhaoze110110@163.com>
2024-03-16 14:58:41 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/STM32TEST1.elf.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Startup/startup_stm32f030f4px.s" "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/cmake-build-debug/CMakeFiles/STM32TEST1.elf.dir/Startup/startup_stm32f030f4px.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F030x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Inc"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Inc"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Src/main.c" "CMakeFiles/STM32TEST1.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Src/main.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Src/stm32f0xx_hal_msp.c" "CMakeFiles/STM32TEST1.elf.dir/Src/stm32f0xx_hal_msp.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Src/stm32f0xx_hal_msp.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Src/stm32f0xx_it.c" "CMakeFiles/STM32TEST1.elf.dir/Src/stm32f0xx_it.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Src/stm32f0xx_it.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Src/syscalls.c" "CMakeFiles/STM32TEST1.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Src/syscalls.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Src/sysmem.c" "CMakeFiles/STM32TEST1.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Src/sysmem.c.obj.d"
+  "C:/Users/quhao/Desktop/DC1/STM32TEST/STM32TEST1/Src/system_stm32f0xx.c" "CMakeFiles/STM32TEST1.elf.dir/Src/system_stm32f0xx.c.obj" "gcc" "CMakeFiles/STM32TEST1.elf.dir/Src/system_stm32f0xx.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/STM32TEST1.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj: \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Src\stm32f0xx_hal_i2c_ex.c \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Inc/stm32f0xx_hal_conf.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_rcc.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_def.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Device\ST\STM32F0xx\Include/stm32f0xx.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Device\ST\STM32F0xx\Include/stm32f030x6.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Include/core_cm0.h \
+ f:\clion_to_keil\gcc\102021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\stdint.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\machine\_default_types.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\sys\features.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\_newlib_version.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\sys\_intsup.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\sys\_stdint.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Include/cmsis_version.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Include/cmsis_compiler.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Include/cmsis_gcc.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\CMSIS\Device\ST\STM32F0xx\Include/system_stm32f0xx.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ f:\clion_to_keil\gcc\102021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_rcc_ex.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_gpio.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_gpio_ex.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_exti.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_dma.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_dma_ex.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_cortex.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_flash.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_flash_ex.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_i2c.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_i2c_ex.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_pwr.h \
+ C:\Users\quhao\Desktop\DC1\STM32TEST\STM32TEST1\Drivers\STM32F0xx_HAL_Driver\Inc/stm32f0xx_hal_pwr_ex.h
